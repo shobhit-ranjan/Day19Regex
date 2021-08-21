799080592f40162c5c1ab0067b12a38c24d8aa0f
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-21 12:45:33 +0530

    UC3 EmailID

--- MainMethodRegEx.java
+++ MainMethodRegEx.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 public class MainMethodRegEx {
 
 	public static void main(String[] args) {
@@ -6,16 +8,25 @@ public class MainMethodRegEx {
 
 	public static boolean firstNameValidation(String name) {
 
-		boolean valid = name.matches( "[A-Z][a-z]*");
+		boolean valid = name.matches("[A-Z][a-z]*");
 
 		return valid;
 	}
+
 	public static boolean lastNameValidation(String name) {
 
-		boolean valid = name.matches( "[A-Z]+([ '-][a-zA-Z]+)*");
+		boolean valid = name.matches("[A-Z]+([ '-][a-zA-Z]+)*");
 
 		return valid;
 	}
-	
-	
+
+	public static boolean EmailValidation(String name) {
+		{
+
+			boolean regex = name.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+
+			return regex;
+		}
+
+	}
 }
